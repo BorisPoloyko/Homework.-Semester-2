2da5098f556da8552d23627db0d4c51363426d2e
BorisPoloyko <43009854+BorisPoloyko@users.noreply.github.com>
2019-04-10 11:12:27 +0300

    Update Source.cpp

--- Poloyko.2019.HW4.Complex/Source.cpp
+++ Poloyko.2019.HW4.Complex/Source.cpp
@@ -1,23 +1,16 @@
 #include <iostream>
-#include "Complex.h"
+#include "Complex.hpp"
 
 using namespace std;
 
 int main()
 {
-	Complex complex1, complex2;
-	int a, b;
-	cout << "Enter real part: ";
-	cin >> a;
-	cout << "Enter imaginary part: ";
-	cin >> b;
-	complex1.setComplex(a, b);
-	complex2.setComplex(a, b);
-	cout << complex1;
-	cout << complex2;
-	Complex complex3;
-	complex3 = complex1 / complex2;
-	cout << complex3;
-	
-	system("pause");
+    Complex complex1, complex2;
+    cin >> complex1;
+    cin >> complex2;
+    cout << complex1;
+    cout << complex2;
+    Complex complex3;
+    complex3 = complex1 / complex2;
+    cout << complex3;
 }
